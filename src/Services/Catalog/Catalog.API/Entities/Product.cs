b8879f141ce782f8876f1104f7ac6bd1556baf27
Yaseen <mohammed17yo@gmail.com>
2022-09-22 12:17:41 +0530

    Null Reference Warning Removed

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -8,11 +8,11 @@ namespace Catalog.API.Entities
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public string Summary { get; set; }
-        public string Description { get; set; }
-        public string ImageFile { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Category { get; set; } = String.Empty;
+        public string Summary { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string ImageFile { get; set; } = String.Empty;
         public decimal Price { get; set; }
     }
 }
